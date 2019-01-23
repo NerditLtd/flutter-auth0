4bbc4873d78ea8f41c5c1cad3fc56ba85917af2a
nerditpty <43914534+NerditLtd@users.noreply.github.com>
2019-01-23 14:39:20 +0200

    Update AuthUser.dart

--- lib/data/AuthUser.dart
+++ lib/data/AuthUser.dart
@@ -10,7 +10,7 @@ class Auth0User {
         idToken = snapshot['id_token'],
         scope = snapshot['scope'],
         expiresDate = DateTime.now()
-            .add(Duration(milliseconds: snapshot['expires_in'] = 0)),
+            .add(Duration(milliseconds: snapshot['expires_in'] = 86400)),
         tokenType = snapshot['token_type'];
 
   toJson() {
